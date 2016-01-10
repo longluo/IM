e7bf7238c406e954f20f5db596ceec2c8106d9b6
luolong <calmbody@163.com>
2016-01-10 21:20:07 +0800

    the Android IM project, just a structure.

---
+++ app/src/main/java/com/luolong/android/im/MainApp.java
@@ -0,0 +1,11 @@
+package com.luolong.android.im;
+
+import android.app.Application;
+
+/**
+ * Created by luolong on 16/1/10.
+ */
+public class MainApp extends Application {
+
+
+}
---
+++ devlib/src/main/java/com/luolong/android/devlib/im/IMManager.java
@@ -0,0 +1,9 @@
+package com.luolong.android.devlib.im;
+
+/**
+ * Created by luolong on 16/1/10.
+ */
+public class IMManager {
+
+
+}
